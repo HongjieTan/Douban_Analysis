410e9bb30b402983350c157f7c4f1370ef26fee3
hongjie_tan <hongjietan@outlook.com>
2020-06-19 09:06:19 +0800

    Update analysis-and-display.py
    修改了数据处理部分

--- analysis-and-display.py
+++ analysis-and-display.py
@@ -3,12 +3,12 @@
 
 # In[33]:
 
-
+import copy
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-path = '../Douban_Analysis/book_list.xlsx'
+path = 'D:/pywork/book_list.xlsx'
 sheet1 = '机器学习'
 sheet2 = '计算机'
 sheet3 = 'linux'
@@ -53,7 +53,7 @@ df1
 # In[89]:
 
 
-print(评价人数大于500的书籍".center(44,'='))
+print("评价人数大于500的书籍".center(44,'='))
 print((sheet1+"类").center(44,'|'))
 print(df1[df1['评价人数']>500][['序号','书名','评分','评价人数']])
 print((sheet2+"类").center(44,'|'))
@@ -67,7 +67,7 @@ print(df4[df4['评价人数']>500][['序号','书名','评分','评价人数']])
 # In[106]:
 
 
-print(评价最好的十本书".center(44,'='))
+print("评价最好的十本书".center(44,'='))
 print((sheet1+"类").center(44,'|'))
 print(df1[df1['评价人数']>0].nlargest(10,'评分')[['序号','书名','评分','评价人数']])
 print((sheet2+"类").center(44,'|'))
@@ -115,23 +115,23 @@ print(df.groupby("类别").describe())
 
 # In[20]:
 
-
+dfc1 = copy.deepcopy(df)
 listBins = [0, 10, 30, 60, 100, 200, 500, 100000]
 listLabels = ['0_10','11_30','31_60','61_100','101_200','201_500','501<']
-df['评价人数'] = pd.cut(df['评价人数'], bins=listBins, labels=listLabels, include_lowest=True)
+dfc1['评价人数'] = pd.cut(dfc1['评价人数'], bins=listBins, labels=listLabels, include_lowest=True)
 
 
 # In[172]:
 
 
 print("各类别在各评价人数段的评分".center(44,'|'),)
-print(pd.crosstab(df.类别,df.评价人数,df.评分,aggfunc='mean'))
+print(pd.crosstab(dfc1.类别,dfc1.评价人数,dfc1.评分,aggfunc='mean'))
 
 
 # In[32]:
 
 
-df_group = pd.crosstab(df.类别,df.评价人数,df.评分,aggfunc='mean')
+df_group = pd.crosstab(dfc1.类别,dfc1.评价人数,dfc1.评分,aggfunc='mean')
 df_group.plot(kind='bar')
 font = fm.FontProperties(fname=r'C:/Windows/Fonts/bahnschrift.ttf')
 plt.xlabel('各类别在各评价人数段评分分布',FontProperties='simhei')
@@ -148,10 +148,10 @@ plt.show()
 
 # In[4]:
 
-
+dfc2 = copy.deepcopy(df)
 listBins = [0, 2, 4, 6, 8, 10]
 listLabels = ['1星级','2星级','3星级','4星级','5星级']
-df['评分'] = pd.cut(df['评分'], bins=listBins, labels=listLabels, include_lowest=True)
+dfc2['评分'] = pd.cut(dfc2['评分'], bins=listBins, labels=listLabels, include_lowest=True)
 
 
 # 
@@ -159,7 +159,7 @@ df['评分'] = pd.cut(df['评分'], bins=listBins, labels=listLabels, include_lo
 # In[8]:
 
 
-df_group = pd.crosstab(df.类别,df.评分)
+df_group = pd.crosstab(dfc2.类别,dfc2.评分)
 print(df_group)
 df_group['5星级'].plot.pie(subplots=True,fontsize=20,counterclock=False,startangle=-270)
 plt.xlabel('5星级的评分分布',FontProperties='simhei')
@@ -174,7 +174,7 @@ plt.show()
 # In[17]:
 
 
-df_group = pd.crosstab(df.评分,df.类别,df.评价人数,aggfunc='sum').sum()
+df_group = pd.crosstab(dfc2.评分,dfc2.类别,dfc2.评价人数,aggfunc='sum').sum()
 df_group.plot.pie(subplots=True,fontsize=20,counterclock=False,startangle=-270)
 plt.xlabel('各类别评价人数分布',FontProperties='simhei')
 plt.xticks(FontProperties='simhei')
